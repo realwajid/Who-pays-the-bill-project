b50e401e83f534d2415d42fa0526349369790741
realwajid <wajidali0403@gmail.com>
2026-04-08 09:23:28 +0500

    Add files via upload

---
+++ src/context/index.jsx
@@ -0,0 +1,84 @@
+import { createContext, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+
+const MyContext = createContext();
+
+const MyProvider = (props) => {
+    const [stage, setStage] = useState(1);
+    const [players, setPlayer] = useState([]);
+    const [result, setResult] = useState('');
+
+    const addPlayerHandler = (name) => {
+        console.log(players)
+        setPlayer(prevPlayers => [...prevPlayers, name]);
+    }
+
+    const removePlayerHandler = (idx) => {
+        let newArray = [...players];
+        newArray.splice(idx, 1)
+        setPlayer(newArray);
+
+    }
+
+
+
+    const nextHandler = () => {
+        if (players.length < 2) {
+            toast.error('You need more than one player',{
+                position:'top-left', 
+                autoClose: 2000
+
+            })
+        } else {
+            //    Next stage
+            setStage(2);
+            setTimeout(() =>{
+                generateLoser();
+
+            },2000)
+            
+
+        }
+    }
+
+    const generateLoser = () => {
+        let result = players[Math.floor(Math.random() * players.length)];
+        setResult(result)
+    }
+
+
+    const resetGameHandler = () => {
+        setStage(1);
+        setPlayer([]);
+        setResult('')
+    }
+
+    return (
+        <>
+
+            <MyContext.Provider value={{
+                // STATE
+                stage: stage,
+                players: players,
+                result: result,
+
+                // METHODS
+                addPlayer: addPlayerHandler,
+                removePlayer: removePlayerHandler,
+                next: nextHandler,
+                generateNewLoser: generateLoser, 
+                resetGame: resetGameHandler
+
+
+            }}>
+                {props.children}
+            </MyContext.Provider>
+            <ToastContainer />
+        </>)
+}
+
+
+export {
+    MyContext,
+    MyProvider
+}
